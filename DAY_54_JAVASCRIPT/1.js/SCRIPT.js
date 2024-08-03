c0237eaf028d51dbff1db96b94b07b9b5584ebc3
SUBHADIP SINHA <119493894+subhadipsinha722133@users.noreply.github.com>
2024-08-03 23:54:43 +0530

    Add files via upload

---
+++ DAY_54_JAVASCRIPT/1.js/SCRIPT.js
@@ -0,0 +1,13 @@
+alert("hellow World");
+
+
+
+
+console.log("hello i am subhadip");
+console.log("hello i am khokon");
+console.log("bjhggjgjh");
+console.log("hello i am subhadip............");
+
+
+var a =prompt(" enter your number")
+console.log("your number is " + a)
---
+++ DAY_54_JAVASCRIPT/2.js/script.js
@@ -0,0 +1,23 @@
+alert("hellow World");
+
+
+
+
+console.log("hello i am subhadip")
+console.log("hello i am khokon")
+console.log("bjhggjgjh")
+console.log("hello i am subhadip............")
+
+
+
+var a =prompt(" enter your number")
+
+var isTrue= confirm ("are you seur?")
+if(isTrue){
+    console.log("yes i am seur")
+}
+
+else{
+    console.log("on i am not sure")
+}
+console.log("your number is " + a)
